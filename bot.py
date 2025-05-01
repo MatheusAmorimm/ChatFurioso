a79396417a633e22816d8e3873512f37f3e0eaed
matheus amorim <mtxdevfrontend@gmail.com>
2025-05-01 02:10:35 -0300

    build: :heavy_plus_sign: adicionando as funcionalidades dos botões

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
 from handlers import start_command
 from handlers import calendary_command
 from handlers import newsletter_command
 from handlers import trivia_command
+from handlers.start import agenda_callback
+from handlers.start import noticias_callback
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -16,6 +18,8 @@ app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("agenda", calendary_command))
 app.add_handler(CommandHandler("noticias", newsletter_command))
 app.add_handler(CommandHandler("curiosidades", trivia_command))
+app.add_handler(CallbackQueryHandler(agenda_callback, pattern="^agenda$"))
+app.add_handler(CallbackQueryHandler(noticias_callback, pattern="^noticias$"))
 
 print("Easy peasy lemon squeezy! #GOFURIA🔥")
 
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,5 @@ from .start import start_command
 from .calendary import calendary_command
 from .newsletter import newsletter_command
 from .trivia import trivia_command
+from .start import agenda_callback
+from .start import noticias_callback
