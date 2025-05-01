605a1f8e4f05439b95ad7c25773b47051476760e
matheus amorim <mtxdevfrontend@gmail.com>
2025-05-01 11:21:26 -0300

    chore: :wrench: ajustando os imports e chamadas dos botões

--- bot.py
+++ bot.py
@@ -2,24 +2,22 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
-from handlers import start_command
-from handlers import calendary_command
-from handlers import newsletter_command
-from handlers import trivia_command
-from handlers.start import agenda_callback
-from handlers.start import noticias_callback
+from handlers.start import start
+from handlers.calendary import calendary_callback
+from handlers.newsletter import newsletter_callback
+from handlers.trivia import trivia_callback
+from handlers.quiz import quiz_callback
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start_command))
-app.add_handler(CommandHandler("agenda", calendary_command))
-app.add_handler(CommandHandler("noticias", newsletter_command))
-app.add_handler(CommandHandler("curiosidades", trivia_command))
-app.add_handler(CallbackQueryHandler(agenda_callback, pattern="^agenda$"))
-app.add_handler(CallbackQueryHandler(noticias_callback, pattern="^noticias$"))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(calendary_callback, pattern="^agenda$"))
+app.add_handler(CallbackQueryHandler(newsletter_callback, pattern="^noticias$"))
+app.add_handler(CallbackQueryHandler(trivia_callback, pattern="^curiosidades$"))
+app.add_handler(CallbackQueryHandler(quiz_callback, pattern='^quiz$'))
 
 print("Easy peasy lemon squeezy! #GOFURIA🔥")
 
