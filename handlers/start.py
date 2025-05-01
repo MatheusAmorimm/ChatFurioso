d3c7fa4dd1d1cb64a0efac333fec1f94cf71579e
matheus amorim <mtxdevfrontend@gmail.com>
2025-05-01 02:09:37 -0300

    feat: :lipstick: estilizando e aprimorando as opções do usuario

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,28 @@
-from telegram import Update
-from telegram.ext import ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes, CallbackContext
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard= [
+        [InlineKeyboardButton("📅 Agenda", callback_data='agenda')],
+        [InlineKeyboardButton("📰 Notícias", callback_data='noticias')],
+        [InlineKeyboardButton("🎲 Curiosidades", callback_data='curiosidades')],
+        [InlineKeyboardButton("🧠 Quiz", callback_data='quiz')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     await update.message.reply_text(
-        "🐾 Fala, Furioso(a)!\n\nEscolha uma opção:\n"
-        "/agenda - Proximos jogos\n"
-        "/noticias - Ultimas Noticias\n"
-        "/quiz - Teste seus conhecimentos furiosos 🔥\n"
-        "/curiosidades - Descubra algo novo"
+        "*Fala, Furioso (a)!*🐾🔥\n\n"
+        "Escolha uma das opções abaixo clicando nos botões:\n"
+        "- (Ou envie o comando se preferir)\n",
+        reply_markup=reply_markup
     )
+
+async def agenda_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text("📅 Aqui está a agenda da FURIA! (em breve...)")
+
+async def noticias_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text("📰 Aqui estão as notícias da FURIA! (em breve...)")
