e3a02b56127bc7d359120714d9378bd2c74e9b31
matheus amorim <mtxdevfrontend@gmail.com>
2025-05-04 18:42:45 -0300

    remove: removendo o quiz, para refazer melhor

--- handlers/quiz.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from handlers.menu import menu_principal
-
-async def quiz_callback(update: Update, context: CallbackContext):
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(text="Aqui teremos um quiz de CS e Furia")
-    await query.message.reply_text("Escolha outra opção:", reply_markup=menu_principal())
